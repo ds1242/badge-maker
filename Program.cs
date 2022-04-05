8c598767b5ec71b52b2fbd060ce98282f89a2f4a
ds1242 <david.shaw1242@gmail.com>
2022-04-05 14:39:44 -0600

    updated program with employee declarations

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-        List<string> employees = GetEmployees();
+        List<Employee> employees = GetEmployees();
 
         PrintEmployees(employees);
        
     }
-    static List<string> GetEmployees() 
+    static List<Employee> GetEmployees() 
     {
         //return a list of employees
-        List<string> employees = new List<string>();
+        List<Employee> employees = new List<Employee>();
         while(true) {
 
             Console.WriteLine("Please enter a name: ");
@@ -25,16 +25,16 @@ namespace CatWorx.BadgeMaker
                 break;
             }
             Employee currentEmployee = new Employee(input, "smith");
-            employees.Add(currentEmployee.GetName());
+            employees.Add(currentEmployee);
         }
         // return the list of employees
         return employees;
     }
-    static void PrintEmployees(List<string> employees)
+    static void PrintEmployees(List<Employee> employees)
     {
         for (int i = 0; i < employees.Count; i++)
         {
-            Console.WriteLine(employees[i]);
+            Console.WriteLine(employees[i].GetName());
         }
     }
   }
