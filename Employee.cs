700e69e9af5a0dd01534d2e629ed102934bb1d15
ds1242 <david.shaw1242@gmail.com>
2022-04-05 14:04:21 -0600

    modularized functions on program, created employee class

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+
+namespace CatWorx.BadgeMaker
+{
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public int Id;
+        public string Photourl;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,18 +7,35 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-        List<string> employees = new List<string>() {"adam", "amy"};
-        employees.Add("barbara");
-        employees.Add("billy");
-        Console.WriteLine("Please enter a name: ");
-        // Get a name from the console and assign it to a variable
-        string input = Console.ReadLine();
-        employees.Add(input);
+        List<string> employees = GetEmployees();
+
+        PrintEmployees(employees);
+       
+    }
+    static List<string> GetEmployees() 
+    {
+        //return a list of employees
+        List<string> employees = new List<string>();
+        while(true) {
+
+            Console.WriteLine("Please enter a name: ");
+            // Get a name from the console and assign it to a variable
+            string input = Console.ReadLine();
+            if( input == "") {
+                break;
+            }
+            Employee currentEmployee = new Employee();
+            employees.Add(input);
+        }
+        // return the list of employees
+        return employees;
+    }
+    static void PrintEmployees(List<string> employees)
+    {
         for (int i = 0; i < employees.Count; i++)
         {
             Console.WriteLine(employees[i]);
         }
-       
     }
   }
 }
