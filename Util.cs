0965dec271cf758dbaefae00879c458d540449e0
ds1242 <david.shaw1242@gmail.com>
2022-04-07 08:58:36 -0600

    setup makeCSV method,  checks/creates data folde, and current writes mocked up data

--- Util.cs
+++ Util.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
@@ -12,6 +13,18 @@ namespace CatWorx.BadgeMaker
             {
                 string template = "{0, -10}\t{1, -20}\t{2}";
                 Console.WriteLine(String.Format(template, employees[i].GetID(), employees[i].GetName(), employees[i].GetPhotoURL()));
+            }
+        }
+        public static void MakeCSV(List<Employee> employees)
+        {
+            if(!Directory.Exists("data"))
+            {
+                // It not, create the directory
+                Directory.CreateDirectory("data");
+            }
+            using (StreamWriter file = new StreamWriter("data/empoyees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoURL");
             }
         }
     }
