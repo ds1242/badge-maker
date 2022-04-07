a3499bdb33ef17214180bcfc6b8726fab67db750
ds1242 <david.shaw1242@gmail.com>
2022-04-07 08:36:33 -0600

    setup Util class and moved print employees method into that class

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace CatWorx.BadgeMaker
     {
         List<Employee> employees = GetEmployees();
 
-        PrintEmployees(employees);
+        Util.PrintEmployees(employees);
        
     }
     static List<Employee> GetEmployees() 
@@ -29,7 +29,7 @@ namespace CatWorx.BadgeMaker
             string lastName = Console.ReadLine();
             Console.WriteLine("Enter ID: ");
             int id = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Entere Photo URL:");
+            Console.WriteLine("Enter Photo URL:");
             string photoUrl = Console.ReadLine();
 
             Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
@@ -37,14 +37,6 @@ namespace CatWorx.BadgeMaker
         }
         // return the list of employees
         return employees;
-    }
-    static void PrintEmployees(List<Employee> employees)
-    {
-        for (int i = 0; i < employees.Count; i++)
-        {
-            string template = "{0, -10}\t{1, -20}\t{2}";
-            Console.WriteLine(String.Format(template, employees[i].GetID(), employees[i].GetName(), employees[i].GetPhotoURL()));
-        }
     }
   }
 }
